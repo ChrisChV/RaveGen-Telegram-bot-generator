6087015221d6436d27ad87b347ae0a57e1145e08
Christofer Chavez <xnpiochv@gmail.com>
2019-04-03 12:17:19 -0500

    test travis

---
+++ tests/test_utils.py
@@ -0,0 +1,22 @@
+import pytest
+import ravegen.Utils.utils as utils
+import ravegen.Utils.commandManager as commandManager
+import ravegen.Utils.sad as sad
+from flaky import flaky
+
+
+@flaky(3,1)
+def test_file_Or_Directory_Exists():
+    test_File_Name = ".testFile"
+    testFile = open(test_File_Name, 'w')
+    testFile.write("Test")
+    testFile.close()
+    assert utils.file_Or_Directory_Exists('./', test_File_Name)
+    commandManager.runRmCommand(".testFile")
+    assert not utils.file_Or_Directory_Exists('./', test_File_Name)
+
+
+
+
+
+
