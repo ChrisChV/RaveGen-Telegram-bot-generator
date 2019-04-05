c05ac08e7d4d9b455ecc41c4701570394b094a5e
ChrisChV <xnpiochv@gmail.com>
2019-04-05 15:12:15 -0500

    adding more test in test_configManager.py

--- tests/test_configManager.py
+++ tests/test_configManager.py
@@ -14,6 +14,29 @@ def setup():
     commandManager.runRmDirCommand(sad._LOG_DIR_NAME_)
     commandManager.runRmDirCommand(sad._MODULES_DIR_)
 
+
+def data_get():
+    trueData = [(sad._CONFIG_RAVEGEN_SECTION_, sad._CONFIG_HOSTING_OPTION_, sad._DEPLOY_HEROKU_OPTION)]
+    falseData_1 = [(sad._CONFIG_RAVEGEN_SECTION_, "TEST", None)]
+    falseData_2 = [("TEST", "TEST", None)]
+    return trueData + falseData_1 + falseData_2
+
+def data_getBoolean():
+    yesData = [("yes", True), ("YES", True), ("YeS", True)]
+    noData = [("no", False), ("NO", False)]
+    return yesData + noData
+
+def data_set():
+    data = [(sad._CONFIG_RAVEGEN_SECTION_, "TEST", "TEST")]
+    data += [(sad._CONFIG_RAVEGEN_SECTION_, sad._CONFIG_HOSTING_OPTION_, "TEST")]
+    data += [("SEC-TEST", "TEST-OP", "TEST")]
+    return data
+
+def data_setSection():
+    trueData = [("TEST-SEC")]
+    falseData = [sad._CONFIG_RAVEGEN_SECTION_]
+    return trueData + falseData
+
 @flaky(3,1)
 def test_getConfig():
     config = configManager.getConfig()
@@ -23,3 +46,30 @@ def test_getConfig():
 def test_verifyConfig():
     config = configManager.getConfig()
     configManager.verifyConfig(config)
+
+@flaky(3,1)
+@pytest.mark.parametrize('section, option, expected', data_get())
+def test_get(section, option, expected):
+    config = configManager.getConfig()
+    assert configManager.get(config, section, option) == expected
+
+@flaky(3,1)
+@pytest.mark.parametrize('value, expected', data_getBoolean())
+def test_getBoolean(value, expected):
+    config = configManager.getConfig()
+    configManager.set(config, "TEST-SEC", "TEST-OP", value)
+    assert configManager.getboolean(config, "TEST-SEC", "TEST-OP") == expected
+    assert configManager.getboolean(config, "TEST", "TEST-OP") is None
+
+@flaky(3,1)
+@pytest.mark.parametrize('section, option, value', data_set())
+def test_set(section, option, value):
+    config = configManager.getConfig()
+    configManager.set(config, section, option, value)
+    assert configManager.get(config, section, option) == value
+
+@flaky(3,1)
+@pytest.mark.parametrize('section', data_setSection())
+def test_setSection(section):
+    config = configManager.getConfig()
+    configManager.setSection(config, section)
