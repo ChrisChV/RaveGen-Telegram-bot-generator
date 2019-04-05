84f7c6037860f8b5550eff881cb79351b680a8e8
ChrisChV <xnpiochv@gmail.com>
2019-04-05 14:28:02 -0500

    release 0.2.3: fixed problems with installation route in ubuntu

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ravegen",
-    version="0.2.2",
+    version="0.2.3",
     scripts=['ravegen/ravegen'],
     author="Christofer Chavez Carazas",
     author_email="xnpiochv@gmail.com",
