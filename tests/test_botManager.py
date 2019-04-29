e6e663863c62d13393b1baab72b0ce604e518cf2
ChrisChV <xnpiochv@gmail.com>
2019-04-29 10:01:32 -0500

    tests in test_botmanager.py fixed

--- tests/test_botManager.py
+++ tests/test_botManager.py
@@ -2,9 +2,11 @@ import pytest
 import os
 import RaveEngine.projectManager as projectManager
 import RaveEngine.botManager as botManager
+import RaveEngine.configManager as configManager
 import Utils.commandManager as commandManager
 from flaky import flaky
 import Utils.sad as sad
+import Utils.utils as utils
 
 @pytest.fixture(autouse=True)
 def setup():
@@ -21,10 +23,16 @@ def data_generateHeaders():
 @flaky(3,1)
 @pytest.mark.parametrize('testFlag', [False, True])
 def test_generateBot(testFlag):
+    if not testFlag:
+        with pytest.raises(SystemExit) as pytest_wrapped_e:
+            botManager.generateBot(testFlag=testFlag)
+        assert pytest_wrapped_e.type == SystemExit
+        config = configManager.getConfig()
+        configManager.set(config, sad._CONFIG_RAVEGEN_SECTION_, sad._CONFIG_DEPLOY_URL_OPTION, "www.test.com")
     botManager.generateBot(testFlag=testFlag)
     assert os.path.exists(sad._OUTPUT_BOT_DIR_)
     assert os.path.exists(sad.OUTPUT_BOT_PATH)
-    headers = botManager._getHeaders()
+    headers = utils._getHeaders()
     if testFlag:
         assert headers[sad._HEADER_TOKEN_FLAG] == sad._STR_TRUE_
     else:
@@ -34,5 +42,5 @@ def test_generateBot(testFlag):
 @pytest.mark.parametrize('header', data_generateHeaders())
 def test_generateHeaders(header):
     botManager.generateBot()
-    headers = botManager._getHeaders()
+    headers = utils._getHeaders()
     assert header in headers
